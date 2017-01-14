f3f9e90de74c0f6b44773be2f45db8e5455c3122
XLILT <xiaolongma002@163.com>
2017-01-14 11:45:17 +0800

    init

---
+++ init_git_cfg.sh
@@ -0,0 +1,20 @@
+#########################################################################
+# File Name: /root/git_init_cfg.sh
+# Author: mxl
+# Mail: xiaolongicx@gmail.com
+# Created Time: Sat 14 Jan 2017 11:21:46 AM CST
+#########################################################################
+#!/usr/bin/env bash
+
+cfg_file=".git/config"
+
+if [ -e $cfg_file ]
+then
+	sed -i 's/url = https:\/\/github.com\//url = git@github.com:/g' .git/config
+	echo "init config successfully."
+else
+
+	echo "file $cfg_file not existed, init config failed!!!"
+fi
+
+
